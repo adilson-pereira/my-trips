20ee1c87cebf240c804be4ee1024495b752731e9
Adilson <adilsonjpj@outlook.com>
2022-03-23 19:24:03 -0300

    Procedimentos da aula 27 finalizados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,16 @@
+import LinkWrapper from 'components/LinkWrapper'
+
+import { InfoOutline } from '@styled-icons/evaicons-outline/InfoOutline'
+
 import dynamic from 'next/dynamic'
 
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
   return (
-    <Map
-      places={[
-        {
-          id: '2',
-          name: 'Teste',
-          slug: 'teste',
-          location: {
-            latitude: -32,
-            longitude: -52
-          }
-        }
-      ]}
-    />
+    <LinkWrapper href="/about">
+      <InfoOutline size={32} aria-label="About" />
+    </LinkWrapper>
   )
+  return <Map />
 }
