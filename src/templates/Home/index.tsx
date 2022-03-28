54e5fc6938a70296a33fad0564be4891f63e31bb
Adilson <adilsonjpj@outlook.com>
2022-03-28 11:27:19 -0300

    Finalizada última parte do curso, porém sem os edits de SEO

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,3 +1,4 @@
+import { NextSeo } from 'next-seo'
 import dynamic from 'next/dynamic'
 import { InfoOutline } from '@styled-icons/evaicons-outline/InfoOutline'
 
@@ -9,6 +10,11 @@ const Map = dynamic(() => import('components/Map'), { ssr: false })
 export default function HomeTemplate({ places }: MapProps) {
   return (
     <>
+      <NextSeo
+        title="My Trips"
+        description="Apenas um projeto para conhecer React, TypeScript e NextJS"
+        canonical="https://my-trips.com.br"
+      />
       <LinkWrapper href="/about">
         <InfoOutline size={32} aria-label="About" />
       </LinkWrapper>
